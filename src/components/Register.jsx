568336dc738aed2f575d63f4bb1a470673c5d419
Aleksander <asava@sibmail.com>
2022-07-11 21:14:28 +0700

    пщшел собаку гулять

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 let Register = () => {
     return (
         <>
-            Registe
+          <Container className="mt-3">
+                <Row>
+                    <Col xs={3}>
+                        <Card className="shadow-lg">
+                            <Card.Header className="p-3" style={{backgroundColor: 'orange' }}>
+                                <h4>Register</h4>
+                            </Card.Header>
+                            <Card.Body>
+                                
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>   
         </>
     )
 };
+
 
 export default Register
