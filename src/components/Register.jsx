2c5d52032bd560a033fe97ae01c645a39339ba9a
Aleksander <asava@sibmail.com>
2022-07-12 15:41:47 +0700

    Реализованна форма

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,65 @@
-import React from "react";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import React, {useState} from "react";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 
 let Register = () => {
+
+    let [state, setState] = useState( {
+        user : {
+            username : '',
+            email : '',
+            password : ''
+        }
+    });
+
+    let updateInput = (e) => {
+        setState({
+            ...state,
+            user : {
+                ...state.user,
+                [e.target.name] : e.target.value
+            }
+        })
+    };
+
+    let register = (e) => {
+        e.preventDefault();
+        console.log(state.user)
+    };
+
+
     return (
         <>
           <Container className="mt-3">
                 <Row>
-                    <Col xs={3}>
+                    <Col md={3}>
                         <Card className="shadow-lg">
-                            <Card.Header className="p-3" style={{backgroundColor: 'orange' }}>
+                            <Card.Header className="p-3" style={{backgroundColor: 'rgb(255,193,7)' }}>
                                 <h4>Register</h4>
                             </Card.Header>
-                            <Card.Body>
+                            <Card.Body style={{backgroundColor: 'rgb(255,255,255)' }}>
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Control
+                                        name="username"
+                                        onChange={updateInput}
+                                        type="text" placeholder="Username"></Form.Control>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Control
+                                        name="email"
+                                        onChange={updateInput} 
+                                        type="text" placeholder="email"></Form.Control>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Control
+                                        name="password"
+                                        onChange={updateInput} 
+                                        type="password" placeholder="password"></Form.Control>
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Button onClick={register} variant="warning" type="submit">Register</Button>
+                                    </Form.Group>
+                                </Form>
                                 
                             </Card.Body>
                         </Card>
